4ad238899dd41a2afc48c332f62af230fd10d118
Daniel Wallace <danielwallace@gtmanfred.com>
2015-05-08 19:25:11 -0500

    don't require login

--- bingo/client/resources.py
+++ bingo/client/resources.py
@@ -1,12 +1,14 @@
 from flask import request, jsonify, render_template, session
 from flask.views import MethodView
-from flask.ext.stormpath import login_required, user, groups_required
+from flask.ext.stormpath import user, groups_required
 
 class Game(MethodView):
     def get(self):
         return render_template('index.html', title='The Game!')
 
 class GameAdmin(MethodView):
-    @groups_required(['admin'])
+    decorators = [
+        groups_required(['admin'])
+    ]
     def get(self):
         return render_template('admin.html', title='The Game!')
